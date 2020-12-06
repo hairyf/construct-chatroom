ce041ecb54fd4e745029f26ff9b3a1fa65ab5f02
RanxinStart <597146348@qq.com>
2020-12-06 18:15:05 +0800

    添加Vant

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import { store, storeSymbol } from './store'
 import router from './routes'
 import App from './App.vue'
-createApp(App).use(router).provide(storeSymbol, store).mount('#app')
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+createApp(App).use(router).use(Vant).provide(storeSymbol, store).mount('#app');
